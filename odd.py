6bf4c821a933c3835d69269d8548b3980f692d5a
Pranay <pranayvjadhao197@gmail.com>
2026-09-15 22:39:59 +0530

    Initial commit

---
+++ odd.py
@@ -0,0 +1,20 @@
+'''d=[1,2,3,4,5,6]
+for i in d:
+    if i%2==1:
+        print (i)
+
+
+#Using function
+
+def odd(d):
+    for i in d:
+        if i%2==1:
+            print(i)
+odd([1,2,3,4,5,6])
+
+def odd_word(s):
+    for i in s:
+        if len(i)%2==1:
+            print(i)
+        
+odd_word(["Back","Welcome","Hi","Prana","Jadhao","Raj"])'''
